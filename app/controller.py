66e6341719cce189f234396a2212e6b286cb6e9e
paulloclara <paulloclara@gmail.com>
2020-03-18 15:52:34 -0300

    :rocket: Add search script

--- app/controller.py
+++ app/controller.py
@@ -1,3 +1,6 @@
+from subprocess import run as run_command
+
+
 class Controller(object):
 
     def __init__(self, view, model):
@@ -16,8 +19,20 @@ class Controller(object):
         self.view.run(events=events)
         self.model.run()
 
+    def find_links(self, url):
+        command = ['scrapy', 'runspider',
+                   f'-s URL={url}', '--nolog',
+                   'app/web-crawler.py']
+        result = run_command(command)
+
+        return result.returncode
+
     def event_copy_link(self, event, link):
         print(link)
 
     def event_search_link(self, event):
-        print('search...')
+        user_input = self.view.main_container.header_container.search_input.get()
+        url = self.model.handle_url(url=user_input)
+
+        self.find_links(url=url)
+        self.model.load_result_file()
--- app/model.py
+++ app/model.py
@@ -1,7 +1,19 @@
 class Model(object):
 
     def __init__(self):
-        pass
+        self.links = []
 
     def run(self):
-        pass
+        self.links = [f'Magnet Link {i}' for i in range(20)]
+
+    def load_result_file(self):
+        with open('app/temp/results.txt', mode='r') as resultFile:
+            self.links = resultFile.readlines()
+
+    def handle_url(self, url):
+        handled_url = url
+
+        if not 'http' in url:
+            handled_url = f'https://{handled_url}'
+
+        return handled_url
